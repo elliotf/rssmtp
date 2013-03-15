66c37927da990417f037f4f0e3086e7e437b508c
Elliot Foster <efoster@firetaco.com>
2013-03-14 22:17:35 -0700

    Make test runs less noisy

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
-  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -19,6 +18,7 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
+  app.use(express.logger('dev'));
   app.use(express.errorHandler());
 });
 
