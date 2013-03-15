356c3d1ef5519fec0f92ead8a28409a15e696771
Elliot Foster <efoster@firetaco.com>
2013-03-15 13:35:15 -0700

    Make test output more scan-able

--- test/routes/index.js
+++ test/routes/index.js
@@ -2,20 +2,23 @@ var helper = require('../../support/spec_helper')
   , expect = require('chai').expect
 ;
 
-describe("GET /", function() {
-  beforeEach(helper.setupRequestSpec);
+describe("Main routes", function() {
+  describe("GET /", function() {
+    beforeEach(helper.setupRequestSpec);
 
-  it("displays the site title", function(done) {
-    this.request
-      .get('/')
-      .end(function(err, res){
-        expect(res.status).to.equal(200);
+    it("displays the site title", function(done) {
+      this.request
+        .get('/')
+        .end(function(err, res){
+          expect(res.status).to.equal(200);
 
-        var $ = helper.$(res.text);
+          var $ = helper.$(res.text);
 
-        expect($('h1').eq(0).text()).to.match(/rss.*email/);
+          expect($('h1').eq(0).text()).to.match(/rss.*email/);
 
-        done();
+          done();
+        });
       });
+    });
   });
 });
