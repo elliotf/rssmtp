6d108498488fc72f2d326bf2501fea91f51ce68f
Elliot Foster <efoster@firetaco.com>
2013-06-20 15:50:42 -0700

    Scraper#feedsForURL initial implementation

--- models/scraper.js
+++ models/scraper.js
@@ -1,4 +1,22 @@
-function Scraper() {
+var _       = require('underscore')
+  , request = require('request')
+;
+
+function Scraper(input) {
+  var args  = input || {};
+  var attrs = ['fetcher'];
+
+  args = _.pick(args, attrs);
+
+  _.defaults(args, {
+    fetcher: request
+  });
+
+  _.defaults(this, args);
 }
+
+Scraper.prototype.feedsForURL = function(url, done) {
+  this.fetcher.get(url, done);
+};
 
 module.exports = Scraper;
--- test/models/scraper.js
+++ test/models/scraper.js
@@ -13,13 +13,46 @@ describe.only("Scraper model", function() {
     this.sinon.stub(request, 'get', function(url, done){
       done(self.request_error, self.request_response, self.request_body);
     });
+
+    self.scrape_error = null;
+    self.scrape_feeds = [];
+
+    this.fakeRequest = {
+      get: this.sinon.stub().callsArgWith(1, self.scrape_error, self.scrape_feeds)
+    };
   });
 
   beforeEach(function() {
-    this.model = new Scraper();
+    this.model = new Scraper({
+      fetcher: this.fakeRequest
+    });
   });
 
   it("can be instantiated", function() {
     expect(this.model).to.exist;
+  });
+
+  describe("when a fetcher is not provided", function() {
+    it("defaults to 'request'", function() {
+      var scraper = new Scraper();
+
+      expect(scraper.fetcher).to.equal(request);
+    });
+  });
+
+  describe("#feedsForURL", function() {
+    it("exists", function() {
+      expect(this.model.feedsForURL).to.be.a('function');
+    });
+
+    it("fetches the provided URL", function(done) {
+      this.model.feedsForURL('http://example.com', function(err, feeds){
+        expect(err).to.not.exist;
+
+        expect(this.fakeRequest.get).to.have.been.calledWith('http://example.com');
+        done();
+
+      }.bind(this));
+    });
   });
 });
