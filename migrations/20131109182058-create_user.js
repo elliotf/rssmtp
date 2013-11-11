a45636a538f92f57f7d820f99bd1e0eb4b63bf91
Elliot Foster <efoster@firetaco.com>
2013-11-10 16:52:51 -0800

    migration script for User model

---
+++ migrations/20131109182058-create_user.js
@@ -0,0 +1,29 @@
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    migration
+      .createTable('users', {
+        id: {
+          type: DataTypes.INTEGER
+          , autoIncrement: true
+          , primaryKey: true
+          , allowNull: false
+        }
+        , email: {
+          type: DataTypes.STRING(2048)
+          , allowNull: false
+        }
+        , created_at: {
+          type: DataTypes.DATE
+          , allowNull: false
+        }
+        , updated_at: {
+          type: DataTypes.DATE
+          , allowNull: false
+        }
+      })
+      .success(done)
+  },
+  down: function(migration, DataTypes, done) {
+    migration.dropTable('users').success(done);
+  }
+}
