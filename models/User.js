b0d50bb72cd99f38469b2fe2b678b17f0b380224
Elliot Foster <efoster@firetaco.com>
2013-11-11 17:20:41 -0800

    User#findOrCreateFromOAUTH (RDBMS)
    YAGNI: don't implement multiple oauth identities for the same RSSMTP
    user until it's needed.  Yay for simpler solutions.

--- models/User.js
+++ models/User.js
@@ -1,11 +1,37 @@
 function init(Sequelize, sequelize, name) {
   return sequelize.define(name, {
     email: Sequelize.STRING(2048)
+    , oauth_provider: Sequelize.STRING(2048)
+    , oauth_id: Sequelize.STRING(2048)
   }, {
     tableName: 'users'
     , instanceMethods: {
     }
     , classMethods: {
+      findOrCreateFromOAUTH: function(oauth, done) {
+        var self = this;
+
+        var data = {
+          oauth_provider: oauth.provider
+          , oauth_id: oauth.id
+        };
+        this.find({
+          where: data
+        })
+          .error(done)
+          .success(function(model){
+            if (model) {
+              return done(null, model, false);
+            }
+            data.email = oauth.emails[0].value;
+            self
+              .create(data)
+              .error(done)
+              .success(function(model){
+                done(null, model, true);
+              })
+          })
+      }
     }
   });
 };
--- test/models/User.js
+++ test/models/User.js
@@ -14,6 +14,63 @@ describe("User model (RDBMS)", function() {
     }).done(done);
   });
 
+  describe("findOrCreateFromOAUTH", function() {
+    beforeEach(function(done) {
+      User.create({
+        email: 'bob@example.com'
+        , oauth_provider: 'oauth_provider_here'
+        , oauth_id: 'oauth_id_here'
+      })
+      .error(done)
+      .success(function(model){
+        this.user = model;
+        done();
+      }.bind(this));
+
+      this.dummyProfileData = {
+        provider: 'dummy_oauth_provider'
+        , id: '3.14159'
+        , displayName: 'Bob Foster'
+        , name: {
+          givenName: 'Bob'
+          , familyName: 'Foster'
+        }
+        , emails: [
+          { value: 'bob.foster@example.com' }
+        ]
+      };
+    });
+
+    describe("when the specified user does NOT exist", function() {
+      it("creates the user", function(done) {
+        User.findOrCreateFromOAUTH(this.dummyProfileData, function(err, user, created) {
+          expect(err).to.not.exist;
+
+          expect(created).to.be.ok;
+          expect(user.email).to.equal("bob.foster@example.com");
+          done();
+        });
+      });
+    });
+
+    describe("when the specified user EXISTS", function() {
+      beforeEach(function() {
+        this.dummyProfileData['provider'] = 'oauth_provider_here';
+        this.dummyProfileData['id'] = 'oauth_id_here';
+      });
+
+      it("returns the existing user", function(done) {
+        User.findOrCreateFromOAUTH(this.dummyProfileData, function(err, user, created) {
+          expect(err).to.not.exist;
+
+          expect(created).not.to.be.ok;
+          expect(user.email).to.equal("bob@example.com");
+          done();
+        });
+      });
+    });
+  });
+
   describe("hasMany feeds", function() {
     beforeEach(function(done) {
       var self = this;
