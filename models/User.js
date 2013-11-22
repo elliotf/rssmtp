509d9c013aa3bf2c213886d2a5124fd844021e62
Elliot Foster <efoster@firetaco.com>
2013-11-22 15:53:57 -0800

    Use model definition pattern from Article

--- models/User.js
+++ models/User.js
@@ -1,41 +1,49 @@
-var classMethods = {};
-var instanceMethods = {};
-
-classMethods.findOrCreateFromOAUTH = function(oauth, done) {
-  var self = this;
-
-  var data = {
-    oauth_provider: oauth.provider
-    , oauth_id: oauth.id
-  };
-  this.find({
-    where: data
-  })
-    .error(done)
-    .success(function(model){
-      if (model) {
-        return done(null, model, false);
-      }
-      data.email = oauth.emails[0].value;
-      self
-        .create(data)
-        .error(done)
-        .success(function(model){
-          done(null, model, true);
-        })
-    })
-}
-
 function init(Sequelize, sequelize, name) {
-  return sequelize.define(name, {
+  var instanceMethods = {}
+    , classMethods    = {}
+  ;
+
+  var attrs = {
     email: Sequelize.STRING(2048)
     , oauth_provider: Sequelize.STRING(2048)
     , oauth_id: Sequelize.STRING(2048)
-  }, {
-    tableName: 'users'
-    , instanceMethods: instanceMethods
-    , classMethods: classMethods
-  });
+  };
+
+  classMethods.findOrCreateFromOAUTH = function(oauth, done) {
+    var self = this;
+
+    var data = {
+      oauth_provider: oauth.provider
+      , oauth_id: oauth.id
+    };
+
+    this.find({
+      where: data
+    })
+      .error(done)
+      .success(function(model){
+        if (model) {
+          return done(null, model, false);
+        }
+        data.email = oauth.emails[0].value;
+        self
+          .create(data)
+          .error(done)
+          .success(function(model){
+            done(null, model, true);
+          })
+      })
+  }
+
+  return sequelize.define(
+    name
+    , attrs
+    , {
+      tableName: 'users'
+      , instanceMethods: instanceMethods
+      , classMethods: classMethods
+    }
+  );
 };
 
 init.relate = function(self, models) {
