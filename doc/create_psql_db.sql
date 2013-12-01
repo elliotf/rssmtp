d1fb68b529eb496d04c569713e7f5c2774b7f23f
Elliot Foster <efoster@firetaco.com>
2013-12-01 13:24:44 -0800

    Comments to show how to use example sql file

--- doc/create_psql_db.sql
+++ doc/create_psql_db.sql
@@ -1,2 +1,4 @@
+-- To create the DB and user in postgres
+-- sudo -u postgres psql -f doc/create_psql_db.sql
 CREATE USER rssmtp WITH PASSWORD 'put_your_own_password_here';
 CREATE DATABASE rssmtp_development WITH OWNER rssmtp;
